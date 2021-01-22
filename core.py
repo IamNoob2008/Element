b6fe7f66108cd737058cadecf5cd41bfa2f246f5
Ritam0604 <72400033+Ritam0604@users.noreply.github.com>
2021-01-22 19:00:12 +0530

    Element

--- core.py
+++ core.py
@@ -21,7 +21,7 @@ def get_prefix(bot, message):
 
 bot = commands.Bot(command_prefix=get_prefix)
 bot.remove_command('help')
-status = cycle([f"{get_prefix}help", "Discord Server, RSGameTech's Official", "https://discord.gg/7GQkt7s6Xx"])
+status = cycle([f"{get_prefix}help", "Discord Server, RSGameTech's Official"])
 
 @bot.event
 async def on_ready():
