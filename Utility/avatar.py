2e623b1ba31d63bc5ea90e8499b507b4280c612f
Ritam0604 <72400033+Ritam0604@users.noreply.github.com>
2021-01-23 13:00:10 +0530

    Element

--- Utility/avatar.py
+++ Utility/avatar.py
@@ -6,12 +6,19 @@ class Avatar(commands.Cog, name="Fun"):
         self.bot = bot
 
     @commands.command()
-    async def av(self, ctx, *, user: discord.Member = None):
+    async def user_av(self, ctx, *, user: discord.Member = None):
         if user is None:
             user = ctx.message.author
         embed = discord.Embed()
         embed.add_field(name=user.name,value=f"[Download]({user.avatar_url})")
         embed.set_image(url=user.avatar_url)
+        embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed)
+
+    @commands.command()
+    async def server_av(self, ctx):
+        embed = discord.Embed()
+        embed.add_field(name=guild.name,value=f"[Download]({guild.icon_url})")
         embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
