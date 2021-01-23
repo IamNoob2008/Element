7d83e2de218f9b97a40f50ae67fcffa78c19346e
Ritam0604 <72400033+Ritam0604@users.noreply.github.com>
2021-01-23 13:36:26 +0530

    Element

--- Fun/F.py
+++ Fun/F.py
@@ -1,19 +1,22 @@
 import discord
 from discord.ext import commands
 
-class F(commands.Cog, name="Extras"):
+class f(commands.Cog, name="Extras"):
     def __init__(self,bot):
         self.bot = bot
 
     @commands.command()
-    async def f(self, ctx, member):
-        embed = discord.Embed(
-            title="F",
-            description=f"{ctx.author.mention} has "
-        )
-        await ctx.send(embed=embed)
+    async def f(self, ctx, *, user: discord.Member = None):
+        if user is None:
+            await ctx.send(f"{ctx.author.mention} has paid the respect")
+        else:
+            embed = discord.Embed(
+                title="F",
+                description=f"{ctx.author.mention} has paid the respect for {user.mention}"
+            )
+            await ctx.send(embed=embed)
             
 
 def setup(bot):
-    bot.add_cog(F(bot))
-    print("F file is loaded!")
+    bot.add_cog(f(bot))
+    print("f file is loaded!")
