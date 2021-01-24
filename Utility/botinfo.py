48d1b4097d0e733d1fb8a72c940c314807968004
Ritam0604 <72400033+Ritam0604@users.noreply.github.com>
2021-01-24 21:13:41 +0530

    Element

--- Utility/botinfo.py
+++ Utility/botinfo.py
@@ -8,9 +8,12 @@ class botinfo(commands.Cog, name="Utility"):
     @commands.command()
     async def botinfo(self, ctx):
         embed = discord.Embed(
-            title = f"",
+            title = f"<:ElementLogo:802919295755223060> Element BOT Info",
+            description =f"Default prefix is --",
             color=0x134F5C
         )
+        embed.set_image(url=ctx.guild.icon_url)
+        embed.add_field(name="Bot Latency", value=f"```{round(self.bot.latency * 1000)}ms```")
         embed.set_footer(text=f"Requested by {ctx.author.name}")
         await ctx.send(embed=embed)
 
