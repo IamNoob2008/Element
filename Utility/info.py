f2da1cd7ddb141503ba330f7e585e1ce8175121b
Ritam0604 <72400033+Ritam0604@users.noreply.github.com>
2021-01-30 20:11:57 +0530

    Element

--- Utility/info.py
+++ Utility/info.py
@@ -13,9 +13,15 @@ class info(commands.Cog, name="Utility"):
             color=0x134F5C
         )
         embed.set_image(url=ctx.guild.icon_url)
-        #embed.add_field(name="Total Server Joined", value=f"```{ctx.guild.joined}```")
-        embed.add_field(name="Bot Latency",value=f"```{round(self.bot.latency * 1000)}ms```",inline=True)
-        embed.add_field(name="ㅤ",value=f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot)",inline=False)
+        fields = [("Bot Latency",f"{round(self.bot.latency* 1000)}ms",True),
+        ("Created By","RSGameTech#9977",True),
+        ("Bot Version","V0.2(Beta)",True),
+        ("Language Using","Python",True),
+        ("Total Server Joined","....",True),
+        ("Total User Useing","....",True),
+        ("ㅤ",f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot)",False)]
+        for name, value, inline in fields:
+            embed.add_field(name=name,value=value,inline=inline)
         embed.set_footer(text=f"Requested by {ctx.author.name}",icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
@@ -33,22 +39,25 @@ class info(commands.Cog, name="Utility"):
         embed.set_footer(text=f"Requested by {ctx.author.name}",icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
-#    @commands.command()
-#    async def ui(self, ctx , *,user: discord.Member = None):
-#        if user is None:
-#            user = ctx.message.author
-#        embed = discord.Embed(
-#            title="User Information",
-#            color=0x134F5C
-#        )
-#        embed.set_thumbnail(url=ctx.author.avatar_url)
-#        embed.add_field(name="Name",value=f"{ctx.author.name}")
-#        embed.add_field(name="ID",value=f"{ctx.author.id}")
-#        embed.add_field(name="Status",value=f"{ctx.author.status}")
-#        #embed.add_field(name="Activity",value=f"{ctx.author.activity}")
-#        embed.add_field(name="Created At",value=f"{ctx.author.created_at.strftime("%d/%m/%Y %H:%M:%S")}")
-#        embed.add_field(name="ㅤ", value=f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot)",inline=False)
-#        await ctx.send(embed=embed)
+    @commands.command()
+    async def ui(self, ctx , *,user: discord.Member = None):
+        if user is None:
+            user = ctx.message.author
+        embed = discord.Embed(
+            title="User Information",
+            color=0x134F5C
+        )
+        embed.set_thumbnail(url=ctx.author.avatar_url)
+        fields = [("Name",ctx.user.name,True),
+        ("ID",ctx.user.id,True),
+        ("Status",ctx.user.status,True),
+        ("Activity",ctx.user.activity,True),
+        ("Created At",ctx.author.created_at.strftime("%d/%m/%Y %H:%M:%S"),True),
+        ("ㅤ",f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot)",False)]
+        for name, value, inline in fields:
+            embed.add_field(name=name,value=value,inline=inline)
+        embed.set_footer(text=f"Requested by {ctx.author.name}",icon_url=ctx.author.avatar_url)
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
