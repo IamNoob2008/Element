7ce9ac2bea57ab5a087edbdd57996fdd33ede5a0
Ritam0604 <72400033+Ritam0604@users.noreply.github.com>
2021-02-04 19:23:23 +0530

    V0.2 (Beta)

--- Utility/info.py
+++ Utility/info.py
@@ -48,12 +48,37 @@ class info(commands.Cog, name="Utility"):
             color=0x134F5C
         )
         embed.set_thumbnail(url=user.avatar_url)
+        for activity in member.activities:
+                    if isinstance(activity, Game):
+                        name = activity.name
+                        Type = "Playing"
+                        Activity = f"{name}"
+                    elif isinstance(activity, Streaming):
+                        name2 = activity.name
+                        name3 = activity.platform
+                        Type = "Streaming"
+                        Activity = f"{name2} on {name3}"
+                    elif isinstance(activity, Spotify):
+                        name4 = activity.title
+                        name5 = activity.artists
+                        Type = "Listening to Spotify"
+                        Activity = f"**Song Name**:{name4}\n**Song Artists:**{name5}"
+                    elif isinstance(activity, CustomActivity):
+                        name6 = activity.name
+                        Type = "Custom Status"
+                        Custom = f"{name6}"
+                    else:
+                        name7 = activity.name
+                        Type =  "Playing"
+                        Activity = f"{name7}"
         fields = [("Name",str(user.name),True),
         ("ID",user.id,True),
         ("Status",str(user.status).capitalize(),True),
-        ("Activity",str(user.activity).split(".")[-1],True),
+        ("Activity",str(user.custom_status)[-1],True),
         ("Created At",user.created_at.strftime("%d/%m/%Y %H:%M:%S"),True),
         ("Joined At",user.joined_at.strftime("%d/%m/%Y %H:%M:%S"),True),
+        ("Activity",f"{Type} {Activity}",True),
+        ("Custom Status",Custom,True),
         ("Links",f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot),<:supporter:775594205186883585>: [Support Server](https://discord.gg/PKP4mG6E3G)",False)]
         for name, value, inline in fields:
             embed.add_field(name=name,value=value,inline=inline)
