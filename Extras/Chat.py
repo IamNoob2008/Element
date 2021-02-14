aa98adc5108798ecd4e97b572585d66125bfaf52
Ritam0604 <72400033+Ritam0604@users.noreply.github.com>
2021-02-14 20:48:09 +0530

    V0.2 (Beta)

--- Extras/Chat.py
+++ Extras/Chat.py
@@ -9,13 +9,13 @@ class Chat(commands.Cog, name="Extras"):
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.content == "??":
-            await message.channel.send(f"{message.author.mention} didn't understand what you told!")
+            await message.channel.send(f"{message.author.Name} didn't understand what you told!")
         elif message.content == "idk":
-            await message.channel.send(f"{message.author.mention} don't know what you asked!'")
+            await message.channel.send(f"{message.author.Name} don't know what you asked!'")
         elif message.content == "hi":
-            await message.channel.send(f"Hi {message.author.mention}, welcome to the chat!")
+            await message.channel.send(f"Hi {message.author.Name}, welcome to the chat!")
         elif message.content == "dead chat":
-            await message.channel.send(f"{message.author.mention}, if it is dead chat then why are you chatting here!")
+            await message.channel.send(f"{message.author.Name}, if it is dead chat then why are you chatting here!")
 
 def setup(bot):
     bot.add_cog(Chat(bot))
