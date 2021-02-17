f75580c9347934a62d4f5391421163b8cd82d5e4
Ritam0604 <72400033+Ritam0604@users.noreply.github.com>
2021-02-17 16:13:29 +0530

    V0.3 (Beta)

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -67,28 +67,28 @@ class moderation(commands.Cog, name="Moderation"):
 
     #Clear
 
-	@commands.command()
-	@commands.has_permissions(manage_messages=True)
-	async def clear(self, ctx, amount: int):
-		embed = discord.Embed(
-			title="Clear News",
-			description="Message has been succesfully clear",
-			color=0xE67E22
-		)
-		embed.add_field(name="ㅤ", value=f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot)")
-		await ctx.channel.purge(limit=amount + 1)
-		await ctx.send(embed=embed)
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount: int):
+        embed = discord.Embed(
+            title="Clear News",
+            description="Message has been succesfully clear",
+            color=0xE67E22
+        )
+        embed.add_field(name="ㅤ", value=f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot)")
+        await ctx.channel.purge(limit=amount + 1)
+        await ctx.send(embed=embed)
 
-	@clear.error
-	async def clear_error(self, ctx, error):
-		if isinstance(error, commands.MissingRequiredArgument):
-			EN = discord.Embed(description=f"{ctx.author.mention}, you have to  specify the amount of message to delete.")
-			EN.add_field(name="ㅤ", value=f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot)")
-			await ctx.send(embed=EN)
-		elif isinstance(error, commands.MissingPermissions):
-			EP = discord.Embed(description=f"{ctx.author.mention}, you don't have the 'Manage Message' permision to use this command.")
-			EP.add_field(name="ㅤ", value=f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot)")
-			await ctx.send(embed=EP)
+    @clear.error
+    async def clear_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            EN = discord.Embed(description=f"{ctx.author.mention}, you have to  specify the amount of message to delete.")
+            EN.add_field(name="ㅤ", value=f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot)")
+            await ctx.send(embed=EN)
+        elif isinstance(error, commands.MissingPermissions):
+            EP = discord.Embed(description=f"{ctx.author.mention}, you don't have the 'Manage Message' permision to use this command.")
+            EP.add_field(name="ㅤ", value=f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot)")
+            await ctx.send(embed=EP)
 
     #Embed
 
@@ -96,9 +96,9 @@ class moderation(commands.Cog, name="Moderation"):
     @commands.has_permissions(manage_messages=True)
     async def embed(self, ctx, title: str = None,*, description: str = None, message=None):
         embed = discord.Embed(title=f"{title}",
-                              description=f"{description}",
-                              color=0xFFFF00
-                              )
+                            description=f"{description}",
+                            color=0xFFFF00
+                            )
         await ctx.send(embed=embed)
 
     @embed.error
