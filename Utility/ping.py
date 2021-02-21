ab5859d8b39b6677c229539116e2b41ce4dc5dde
Ritam0604 <72400033+Ritam0604@users.noreply.github.com>
2021-02-21 11:30:34 +0530

    V0.3 (Beta)

--- Utility/ping.py
+++ Utility/ping.py
@@ -8,10 +8,9 @@ class ping(commands.Cog, name="Utility"):
 	@commands.command()
 	async def ping(self, ctx):
 		await ctx.send("Pinging <a:WindowsDotLoading:809471466311516211>")
-		await ctx.edit
 		embed = discord.Embed(title="Pong!",description=f"Your latency is {round(self.bot.latency * 1000)}ms",color=0x00FF00)
 		embed.add_field(name="ㅤ",value=f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot)")
-		await ctx.send(embed=embed)
+		await ctx.edit(embed=embed)
 
 def setup(bot):
     bot.add_cog(ping(bot))
