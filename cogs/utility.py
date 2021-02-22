48b4a25a0fe91af70ffb950570c08f8512ed98b9
RS GameTech <rsgame0604@gmail.com>
2021-02-22 07:23:49 +0000

    V0.4 (Beta)

--- cogs/utility.py
+++ cogs/utility.py
@@ -30,14 +30,14 @@ class util(commands.Cog, name="Utility"):
 
     #Ping
 
-	@commands.command()
-	async def ping(self, ctx):
-		msg = await ctx.send("Pinging <a:WindowsDotLoading:809471466311516211>")
-		await ctx.send(msg)
-		await asyncio.sleep(5)
-		embed = discord.Embed(title="Pong!",description=f"Your latency is {round(self.bot.latency * 1000)}ms",color=0x00FF00)
-		embed.add_field(name="Links", value=f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot) | <:supporter:775594205186883585>: [Support Server](https://discord.gg/PKP4mG6E3G)", inline=False)
-		await msg.edit(content = "",embed=embed)
+		@commands.command()
+		async def ping(self, ctx):
+			msg = await ctx.send("Pinging <a:WindowsDotLoading:809471466311516211>")
+			await ctx.send(msg)
+			await asyncio.sleep(5)
+			embed = discord.Embed(title="Pong!",description=f"Your latency is {round(self.bot.latency * 1000)}ms",color=0x00FF00)
+			embed.add_field(name="Links", value=f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot) | <:supporter:775594205186883585>: [Support Server](https://discord.gg/PKP4mG6E3G)", inline=False)
+			await msg.edit(content = "",embed=embed)
 
 def setup(bot):
     bot.add_cog(util(bot))
