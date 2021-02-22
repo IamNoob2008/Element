bc7d96952ec2935cb7ee6a9ff57f6199e766bc48
Ritam0604 <72400033+Ritam0604@users.noreply.github.com>
2021-02-22 12:59:32 +0530

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -35,7 +35,7 @@ class util(commands.Cog, name="Utility"):
 	    msg = await ctx.send("Pinging <a:WindowsDotLoading:809471466311516211>")
 	    await ctx.send(msg)
 	    await asyncio.sleep(5)
-            embed = discord.Embed(title="Pong!",description=f"Your latency is {round(self.bot.latency * 1000)}ms",color=0x00FF00)
+        embed = discord.Embed(title="Pong!",description=f"Your latency is {round(self.bot.latency * 1000)}ms",color=0x00FF00)
 	    embed.add_field(name="Links", value=f"<:DiscordBOT:801303572183777280>: [Invite Bot](https://discord.com/api/oauth2/authorize?client_id=790832263260012573&permissions=8&scope=bot) | <:supporter:775594205186883585>: [Support Server](https://discord.gg/PKP4mG6E3G)", inline=False)
 	    await msg.edit(content = "",embed=embed)
 
