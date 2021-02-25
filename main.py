59dd0490d281d135b360726e46ed056b760affe2
Ritam0604 <72400033+Ritam0604@users.noreply.github.com>
2021-02-25 07:39:04 +0530

    V0.4 (Beta)

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import os
 import traceback
 import json
 import jishaku
+import aiohttp
+import selenium
+import spotipy
 from discord.ext import commands, tasks
 from itertools import cycle
 from dotenv import load_dotenv
